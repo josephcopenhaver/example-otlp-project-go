44f156bf437fb4f5d814c999292f6b82c3e613b9
Joseph Copenhaver <joseph.copenhaver@gmail.com>
2024-01-05 02:20:24 -0600

    k3d bb

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,29 +4,13 @@ set -euxo pipefail
 main() {
     local gitsha="$(git log --pretty=format:%H -n 1)"
 
-    local pids=()
-    sleep 0 &
-    pids+=("$!")
-    kind load docker-image "josephcopenhaver/middle-service:$gitsha" &
-    pids+=("$!")
-    kind load docker-image "josephcopenhaver/end-service:$gitsha" &
-    pids+=("$!")
-    
-    set +exo pipefail
-    local cec=0
-    for pid in ${pids[*]}; do
-        wait $pid
-        local ec="$?"
-        if [[ $ec -ne 0 ]]; then
-            cec=$ec
-        fi
-    done
-    pids=()
-    set -exo pipefail
-    if [[ $cec -ne 0 ]]; then
-        exit $cec
-    fi
+    local registryhost="k3d-registry:5000"
 
+    docker tag "josephcopenhaver/middle-service:$gitsha" "$registryhost/josephcopenhaver/middle-service:$gitsha"
+    docker tag "josephcopenhaver/end-service:$gitsha" "$registryhost/josephcopenhaver/end-service:$gitsha"
+
+    docker push "$registryhost/josephcopenhaver/middle-service:$gitsha"
+    docker push "$registryhost/josephcopenhaver/end-service:$gitsha"
 
     sleep 0 &
     pids+=("$!")
